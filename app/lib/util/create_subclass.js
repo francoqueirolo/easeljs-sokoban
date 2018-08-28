f71ab7084b277cd87df2d22f8fd825580c804d6b
Franco Queirolo <fgqueirolo@gmail.com>
2018-08-27 22:26:06 -0500

    Initial commit

---
+++ app/lib/util/create_subclass.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = createSubclass;
+
+function createSubclass(Superclass, name, methods) {
+    var Subclass;
+
+    eval('Subclass = function ' + name + '(){ this.initialize.apply(this, arguments) }');
+    Subclass.prototype = Object.create(Superclass.prototype);
+
+    for (var key in methods) {
+        if (methods.hasOwnProperty(key))
+            Subclass.prototype[key] = methods[key];
+    }
+
+    return Subclass;
+}
